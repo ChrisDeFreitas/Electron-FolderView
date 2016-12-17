fd321394b118a7870ab1fa9cc09e0e677673bc4c
ChrisDeFreitas <chrisd@europa.com>
2016-12-16 16:40:29 -0800

    ignore dev files

---
+++ gitshell.bat
@@ -0,0 +1,4 @@
+rem @echo off
+rem git clone -v --progress https://github.com/ChrisDeFreitas/Electron-FolderView
+
+c:\Windows\System32\cmd.exe /c "start C:\Users\chris\AppData\Local\GitHub\PortableGit_d7effa1a4a322478cd29c826b52a0c118ad3db11\git-bash.exe && exit"
