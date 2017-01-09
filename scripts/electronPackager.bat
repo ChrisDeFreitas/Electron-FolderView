f9227e737ef4c42026f21a68c6f0ebd0989d73b7
ChrisDeFreitas <chrisd@europa.com>
2017-01-09 02:46:33 -0800

    fixed icon path for packager

--- scripts/electronPackager.bat
+++ scripts/electronPackager.bat
@@ -1,6 +1,6 @@
 @echo.
 electron-packager . FolderView --out="dist" --no-prune --overwrite ^
-  --icon="./lib/Softskin-Series-Folder-Folder-Season-Pack.ico" ^
+  --icon="./resources/Softskin-Series-Folder-Folder-Season-Pack.ico" ^
   --win32metadata.ProductName="FolderView" ^
   --win32metadata.InternalName="FolderView" ^
   --win32metadata.FileDescription="View and play folder contents"
